b2d31226b07c980673203cf23eee6990b10156e8
jjkoh95 <smarter_koh@hotmail.com>
2017-12-27 14:44:35 +0800

    Fixed some bugs
    - When users have more than 1,000 posts

--- instastalk.py
+++ instastalk.py
@@ -14,6 +14,9 @@ false = False
 URL = 'https://www.instagram.com/'
 QUERYID = 17888483320059182
 AJAXURL = URL + 'graphql/query/?query_id={0}&variables=%7B"id"%3A"{1}"%2C"first"%3A{2}%2C"after"%3A"{3}"%7D'
+TARGETID = 0
+TOTALPOSTS = 0
+NUM = 500
 
 
 def getWebData(url):
@@ -33,17 +36,19 @@ def getWebData(url):
 	# if not found		
 	return
 
+
 def getLatestFilename():
 	'''
 		to get the latest file (prevent redundant download)
 		assume that you are the directory
 	'''
 	fileList = os.listdir()
-	fl = sorted(fileList, reverse=True)
+	fileList.sort(reverse=True)
 	# can use comparison directly
 	# >>> fileList[1] < fileList[0]
 	# >>> True
-	return fl[0]
+	return fileList[0]
+
 
 def getAPIData(url):
 	apiData = urllib.request.urlopen(url)
@@ -51,14 +56,22 @@ def getAPIData(url):
 	apiData = apiData.decode('utf-8')
 	return json.loads(apiData)
 
-def getDataEdges():
+
+def getIDandTotalPosts():
 	webData = getWebData(URL + TARGETUSER)
+	global TARGETID
 	TARGETID = webData['entry_data']['ProfilePage'][0]['user']['id']
-	totalPosts = webData['entry_data']['ProfilePage'][0]['user']['media']['count']
+	global TOTALPOSTS
+	TOTALPOSTS = webData['entry_data']['ProfilePage'][0]['user']['media']['count']
+
+
+def getDataEdges(end_cursor):
 	global AJAXURL
-	AJAXURL = AJAXURL.format(QUERYID, TARGETID, totalPosts, "")
+	AJAXURL = AJAXURL.format(QUERYID, TARGETID, NUM, end_cursor)
 	apiData = getAPIData(AJAXURL)
-	return apiData['data']['user']['edge_owner_to_timeline_media']['edges']
+	apiData = apiData['data']['user']['edge_owner_to_timeline_media']
+	return apiData['page_info']['end_cursor'], apiData['edges']
+
 
 def downloadPostByType(dataNode, time, typename):
 	try:
@@ -83,45 +96,46 @@ def downloadPostByType(dataNode, time, typename):
 		print(e)
 		print("error, retrying")
 		return downloadPostByType(dataNode, time, typename)
-		
+
 def main():
-	dataEdges = getDataEdges()
+	getIDandTotalPosts()
+	# TARGETID, TOTALPOSTS
+	end_cursor = ""
+	counter = 0
 	if not os.path.exists(TARGETUSER):
-		# if does not exist, download all from the beginning
 		os.mkdir(TARGETUSER)
 		os.chdir(TARGETUSER)
-		i = len(dataEdges) - 1
-		while i >= 0:
-			time = dataEdges[i]['node']['taken_at_timestamp']
-			time = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d--%H-%M-%S')
-			print("downloading post at {0}".format(time))
-			downloadPostByType(dataEdges[i]['node'], time, dataEdges[i]['node']['__typename'])
-			i -= 1
-
-	else:
-		# else UPDATE as necessary
-		os.chdir(TARGETUSER)
-		latestTime = getLatestFilename()
-		i = 0
-		while i < len(dataEdges):
-			time = dataEdges[i]['node']['taken_at_timestamp']
-			time = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d--%H-%M-%S')
-			if time > latestTime:
+		while counter < TOTALPOSTS:
+			end_cursor, dataEdges = getDataEdges(end_cursor)
+			counter += NUM
+			i = len(dataEdges) - 1
+			while i >= 0:
+				time = dataEdges[i]['node']['taken_at_timestamp']
+				time = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d--%H-%M-%S')
 				print("downloading post at {0}".format(time))
 				downloadPostByType(dataEdges[i]['node'], time, dataEdges[i]['node']['__typename'])
-				i += 1
-			else:
-				return
+				i -= 1
+	else:
+		os.chdir(TARGETUSER)
+		while counter < TOTALPOSTS:
+			end_cursor, dataEdges = getDataEdges(end_cursor)
+			counter += NUM
+			latestTime = getLatestFilename()
+			i = 0
+			while i < len(dataEdges):
+				time = dataEdges[i]['node']['taken_at_timestamp']
+				time = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d--%H-%M-%S')
+				if time > latestTime:
+					print("downloading post at {0}".format(time))
+					downloadPostByType(dataEdges[i]['node'], time, dataEdges[i]['node']['__typename'])
+					i += 1
+				else:
+					return
 
 if __name__ == '__main__':
 	global TARGETUSER
 	TARGETUSER = sys.argv[1]
 	main()
-
-
-
-
-
 
 
 
